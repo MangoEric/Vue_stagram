4e725538aca10c8627acdc060ea1b1155439a4b3
Jong <fgh1937@yahoo.com>
2023-04-12 23:05:49 +0900

    01 - layout , data binding

---
+++ src/components/ContainerCom.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <Post
+      v-for="(a, i) in 게시물" :key="i"
+     :게시물="게시물[i]" />
+</div>
+</template>
+
+<script>
+import Post from './PostCom.vue';
+
+export default {
+  components :{
+    Post,
+  },
+  props :{
+    게시물 : Array,
+  }
+}
+</script>
+
+<style>
+
+</style>
