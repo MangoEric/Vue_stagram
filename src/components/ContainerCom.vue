10c5a66dad019f28548f527a38350b85bfb3d3d5
Jong <fgh1937@yahoo.com>
2023-04-16 13:38:40 +0900

    05 - filter

--- src/components/ContainerCom.vue
+++ src/components/ContainerCom.vue
@@ -7,11 +7,7 @@
     <div v-if="step == 1">
       <div class="upload-image" :style="`background-image:url(${이미지})`"></div>
         <div class="filters">
-          <div class="filter-1"></div>
-          <div class="filter-1"></div>
-          <div class="filter-1"></div>
-          <div class="filter-1"></div>
-          <div class="filter-1"></div>
+          <FilterBox :필터="필터" :이미지="이미지" v-for="필터 in 필터들" :key="필터"></FilterBox>
         </div>
       </div>
     <!-- 글작성페이지 -->
@@ -26,10 +22,19 @@
 
 <script>
 import Post from './PostCom.vue';
+import FilterBox from './FilterBox.vue'
 
 export default {
+  data(){
+    return{
+      필터들 :[ "aden", "_1977", "brannan", "brooklyn", "clarendon", "earlybird", "gingham", "hudson", 
+"inkwell", "kelvin", "lark", "lofi", "maven", "mayfair", "moon", "nashville", "perpetua", 
+"reyes", "rise", "slumber", "stinson", "toaster", "valencia", "walden", "willow", "xpro2"]
+    }
+  },
   components :{
     Post,
+    FilterBox,
   },
   props :{
     게시물 : Array,
