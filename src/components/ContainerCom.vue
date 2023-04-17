c57004e82d242eedfb7feb8a03a64541bf77f9ca
Jong <fgh1937@yahoo.com>
2023-04-17 21:17:23 +0900

    07 - mitt

--- src/components/ContainerCom.vue
+++ src/components/ContainerCom.vue
@@ -5,17 +5,16 @@
     </div>
     <!-- 필터선택페이지 -->
     <div v-if="step == 1">
-      <div class="upload-image" :style="`background-image:url(${이미지})`"></div>
+      <div :class="선택한필터" class="upload-image" :style="`background-image:url(${이미지})`"></div>
         <div class="filters">
           <FilterBox :필터="필터" :이미지="이미지" v-for="필터 in 필터들" :key="필터">
             <template v-slot:a>{{필터}}</template>
-            <template v-slot:default="작명"><span>{{ 작명.msg }}</span></template>
           </FilterBox>
         </div>
       </div>
     <!-- 글작성페이지 -->
     <div v-if="step == 2">
-      <div class="upload-image" :style="`background-image:url(${이미지})`"></div>
+      <div :class="선택한필터" class="upload-image" :style="`background-image:url(${이미지})`"></div>
       <div class="write">
         <textarea @input="$emit('write', $event.target.value)" class="write-box">write!</textarea> 
       </div>
@@ -32,8 +31,14 @@ export default {
     return{
       필터들 :[ "aden", "_1977", "brannan", "brooklyn", "clarendon", "earlybird", "gingham", "hudson", 
 "inkwell", "kelvin", "lark", "lofi", "maven", "mayfair", "moon", "nashville", "perpetua", 
-"reyes", "rise", "slumber", "stinson", "toaster", "valencia", "walden", "willow", "xpro2"]
+"reyes", "rise", "slumber", "stinson", "toaster", "valencia", "walden", "willow", "xpro2"],
+      선택한필터 : '',
     }
+  },
+  mounted(){
+    this.emitter.on('박스클릭함',(a)=>{
+      this.선택한필터 = a;
+    })
   },
   components :{
     Post,
--- src/components/FilterBox.vue
+++ src/components/FilterBox.vue
@@ -1,14 +1,17 @@
 <template>
-  <div :class="필터 + ' filter-item'" :style="`background-image:url(${이미지})`">
+  <div @click="fire" :class="필터 + ' filter-item'" :style="`background-image:url(${이미지})`">
     <slot name="a"></slot>
-    <!-- 부모한테 보낼때 -->
-    <slot :msg="msg"></slot>
   </div> 
 </template>
 
 <script>
 export default {
   name : 'FilterBox',
+  methods : {
+    fire(){
+      this.emitter.emit('박스클릭함',this.필터);
+    },
+  },
   props :{
     이미지 : String,
     필터 : String,
