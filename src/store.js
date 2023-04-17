46b66b050a93a963073b4fbfbdc27e1959c779c4
Jong <fgh1937@yahoo.com>
2023-04-17 22:29:59 +0900

    09 - vuex state, mutations

--- src/store.js
+++ src/store.js
@@ -4,8 +4,20 @@ const store = createStore({
   state(){
     return {
       name : 'ko',
+      age : 20,
     }
   },
+  mutations :{
+    chageName(state){
+      state.name = 'wow'
+    },
+    addAge(state){
+      state.age++
+    },
+    ten(state, data){
+      state.age += data
+    }
+  }
 })
 
 export default store
